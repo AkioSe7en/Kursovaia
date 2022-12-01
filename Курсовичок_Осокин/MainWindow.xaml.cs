7ab5163f4c343295c70abb0d96c51e7942984db0
Akio <myEmail@example.com>
2022-12-02 00:49:25 +0700

    Добавление вкладок Заполнение вкладки "Функционал"

--- Курсовичок_Осокин/MainWindow.xaml.cs
+++ Курсовичок_Осокин/MainWindow.xaml.cs
@@ -9,41 +9,77 @@
         public MainWindow()
         {
             InitializeComponent();
+            if (db.Database.CanConnect())
+                CheckBD.Text = "БД создана";
+            else
+            {
+                CheckBD.Text = "БД не создана";
+            }
         }
-        
+
         private void CreateBD_OnClick(object sender, RoutedEventArgs e)
         {
-            ApplicationContext db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var Prof = new List<Professions>
+            if (db.Database.EnsureCreated() == false)
             {
-                new () {Prof_Name = "Кассир"},
-                new () {Prof_Name = "Грузчик"},
-                new () {Prof_Name = "Маляр"},
-                new () {Prof_Name = "Учитель"},
-                new () {Prof_Name = "Кассир-администратор"},
+                MessageBox.Show("БД уже создана. Если вы хотите ее пересоздать - удалите её и создайте снова.");   
+            }
+            else
+            {
+                /*  var Prof = new List<Professions>
+            {
+                new() { Prof_Name = "Кассир-администратор" },
+                new() { Prof_Name = "Кассир" },
+                new() { Prof_Name = "Грузчик" },
+                new() { Prof_Name = "Маляр" },
+                new() { Prof_Name = "Учитель" }
             };
             db.Professions.AddRange(Prof);
             db.SaveChanges();
 
-            Applicants User = new Applicants {Familia = "Осокин", Name = "Тимофей", Otchestvo = "Сергеевич", Email = "tim_osokin@mail.ru", Professions = db.Professions.Where(u => u.Prof_Name == "Маляр").ToList()[0]};
-            Diploma Diplom = new Diploma{ID_Dipl = "1334501", Institution = "БГК", Specialties = "Информационные системы", Date_Receiving = new DateTime(2023, 05, 29).ToShortDateString(), Applicants = User};
+            Applicants User = new Applicants
+            {
+                Familia = "Осокин", Name = "Тимофей", Otchestvo = "Сергеевич", Email = "tim_osokin@mail.ru",
+                Professions = db.Professions.Where(u => u.Prof_Name == "Кассир-администратор").ToList()[0]
+            };
+            Diploma Diplom = new Diploma
+            {
+                ID_Dipl = "1334501", Institution = "БГК", Specialties = "Информационные системы",
+                Date_Receiving = new DateTime(2023, 05, 29).ToShortDateString(), Applicants = User
+            };
             db.Applicants.Add(User);
             db.Diploma.Add(Diplom);
             db.SaveChanges();
-            
-            Employers employers = new Employers{FIO = "Куксин П.С.", Name_Org = "ООО Куриное царство"};
-            Vacancy vacancy = new Vacancy{Employers = employers, Professions = db.Professions.Where(u => u.Prof_Name == "Кассир").ToList()[0], Salary = 25000.00};
-            db.Employers.Add(employers);
-            db.Vacancy.Add(vacancy);
-            db.SaveChanges();
-            var z1 = db.Applicants.ToList();
-            foreach (var z in z1)
+
+            Employers employers = new Employers { FIO = "Куксин П.С.", Name_Org = "ООО Куриное царство" };
+            Vacancy vacancy = new Vacancy
             {
-                MessageBox.Show($"{z.Familia} {z.Name} {z.Otchestvo} {z.Professions.Prof_Name}");
+                Employers = employers, Professions = db.Professions.Where(u => u.Prof_Name == "Кассир").ToList()[0],
+                Salary = 25000.00
+            };
+            db.Employers.Add(employers);
+            db.Vacancy.Add(vacancy);*/
+                db.SaveChanges();
+                CheckBD.Text = "БД создана";
+                MessageBox.Show("БД создана"); 
+            } 
+        }
+
+        private void DeleteBD_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (db.Database.EnsureDeleted() == false)
+            {
+               MessageBox.Show("База данных не найдена");
             }
+            else
+            {
+                CheckBD.Text = "БД не создана";
+                MessageBox.Show("БД удалена");
+            }
+            
+        }
+
+        private void UIElement_OnFocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
 
         }
     }

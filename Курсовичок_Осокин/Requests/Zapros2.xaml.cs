4251474799eed16d26b61a6485983594e256d239
Akio <myEmail@example.com>
2022-12-05 18:00:22 +0700

    Ура полностью рабочий запрос 2

--- Курсовичок_Осокин/Requests/Zapros2.xaml.cs
+++ Курсовичок_Осокин/Requests/Zapros2.xaml.cs
@@ -1,11 +1,44 @@
-﻿using System.Windows;
+﻿namespace Куровичок_Осокин.Requests;
 
-namespace Куровичок_Осокин.Requests;
+// public class Zapros
+// {
+//     public Guid Id { get; set; }
+//     public string FIO { get; set; }
+//     public string? Phone { get; set; } 
+//     public string? Email { get; set; }
+//     public string? Address { get; set; }
+//     public string Org { get; set; }
+//     public bool Activ { get; set; }
+//     public DateTime? DateDelete { get; set; }
+// }
 
 public partial class Zapros2 : Window
 {
-    public Zapros2()
+    ApplicationContext db = new ApplicationContext();
+    private string Like;
+    public Zapros2(string Like)
     {
         InitializeComponent();
+        this.Like = Like;
+        CheckBD();
+    }
+
+    void CheckBD()
+    {
+        var Zapros = db.Employers
+            .Where(p=>EF.Functions.Like(p.Address, "%"+Like+"%")).ToList();
+        DataGrid.ItemsSource = Zapros;
+    }
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        db.SaveChanges();
+        CheckBD();
+
+    }
+
+    private void DataGrid_OnCellEditEnding(object? sender, DataGridCellEditEndingEventArgs e)
+    {
+        db.SaveChanges();
+        CheckBD();
     }
 }
---
+++ Курсовичок_Осокин/TempWindows/TempZap2.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Куровичок_Осокин.Requests;
+
+namespace Куровичок_Осокин.TempWindows;
+
+public partial class TempZap2 : Window
+{
+    public TempZap2()
+    {
+        InitializeComponent();
+    }
+    private void Ok_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Zapros2 zap = new Zapros2(VacText.Text); 
+        zap.Show();
+    }
+}

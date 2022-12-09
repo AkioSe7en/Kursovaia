d76e4b5ce566e10221a1515f2203db95bdebd3ef
Akio <myEmail@example.com>
2022-12-10 00:06:39 +0700

    Создание формы с выводом всех работодателей

--- Курсовичок_Осокин/Forms/EmpAll.xaml.cs
+++ Курсовичок_Осокин/Forms/EmpAll.xaml.cs
@@ -4,8 +4,38 @@ namespace Куровичок_Осокин.Forms;
 
 public partial class EmpAll : Window
 {
+    ApplicationContext db = new ApplicationContext();
     public EmpAll()
     {
         InitializeComponent();
+        CheckBD();
+    }
+    void CheckBD()
+    {
+        var Zapros = db.Employers.ToList();
+        DataGrid.ItemsSource = Zapros;
+    }
+
+    private void Create_OnClick(object sender, RoutedEventArgs e)
+    {
+        Zap2_Add add = new Zap2_Add(db);
+        add.ShowDialog();
+        CheckBD();
+    }
+
+    private void Delete_OnClick(object sender, RoutedEventArgs e)
+    {
+        for (var i = 0; i < DataGrid.SelectedItems.Count; i++)
+        {
+            db.Employers.Remove((Employers)DataGrid.SelectedItems[i]!);
+        }
+        db.SaveChanges();
+        CheckBD();
+    }
+
+    private void DataGrid_OnRowEditEnding(object? sender, DataGridRowEditEndingEventArgs e)
+    {
+        db.SaveChanges();
+        CheckBD();
     }
 }

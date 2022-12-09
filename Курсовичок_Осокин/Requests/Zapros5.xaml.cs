186471e1f9395f38f58f4088c5d5ded23e76af86
Akio <myEmail@example.com>
2022-12-09 18:25:50 +0700

    Запрос 5 создан

--- Курсовичок_Осокин/Requests/Zapros5.xaml.cs
+++ Курсовичок_Осокин/Requests/Zapros5.xaml.cs
@@ -4,8 +4,38 @@ namespace Куровичок_Осокин.Requests;
 
 public partial class Zapros5 : Window
 {
+    ApplicationContext db = new ApplicationContext();
     public Zapros5()
     {
         InitializeComponent();
+        CheckBD();
+    }
+    void CheckBD()
+    {
+        var Zapros = db.Applicants.Where(p=>p.Activ==true).ToList();
+        DataGrid.ItemsSource = Zapros;
+    }
+    
+    private void Create_OnClick(object sender, RoutedEventArgs e)
+    {
+        Zap3_Add add = new Zap3_Add(db);
+        add.ShowDialog();
+        CheckBD();
+    }
+
+    private void Delete_OnClick(object sender, RoutedEventArgs e)
+    {
+        for (var i = 0; i < DataGrid.SelectedItems.Count; i++)
+        {
+            db.Applicants.Remove((Applicants)DataGrid.SelectedItems[i]!);
+        }
+        db.SaveChanges();
+        CheckBD();
+    }
+
+    private void DataGrid_OnRowEditEnding(object? sender, DataGridRowEditEndingEventArgs e)
+    {
+        db.SaveChanges();
+        CheckBD();
     }
 }

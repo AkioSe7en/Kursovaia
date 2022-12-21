6250a1f79179b75946f17d2ab9ffa65821174e76
Akio <myEmail@example.com>
2022-12-21 22:08:32 +0700

    Сделана форма профессии

--- Курсовичок_Осокин/Forms/Prof.xaml.cs
+++ Курсовичок_Осокин/Forms/Prof.xaml.cs
@@ -4,8 +4,34 @@ namespace Куровичок_Осокин.Forms;
 
 public partial class Prof : Window
 {
+    ApplicationContext db = new ApplicationContext();
     public Prof()
     {
         InitializeComponent();
+        CheckBD();
+    }
+
+    void CheckBD()
+    {
+        DataGrid.ItemsSource = db.Professions.ToList();  
+    }
+    
+    private void DataGrid_OnRowEditEnding(object? sender, DataGridRowEditEndingEventArgs e)
+    {
+        db.Professions.Update((Professions)DataGrid.SelectedItem);
+        db.SaveChanges();
+        CheckBD();
+    }
+
+    private void DataGrid_OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Delete)
+        {
+            foreach (var z in DataGrid.SelectedItems)
+            {
+                db.Professions.Remove((Professions)z);  
+            }
+            db.SaveChanges();
+        }
     }
 }
--- Курсовичок_Осокин/Tables/Professions.cs
+++ Курсовичок_Осокин/Tables/Professions.cs
@@ -2,7 +2,8 @@
 
 public class Professions
 {
+    public Guid ProfessionsID { get; set; }
     public string Prof_Name { get; set; }
-    // public List<Vacancy> Vacancies { get; set; } = new();
-    // public List<Applicants> ApplicantsList { get; set; } = new();
+   // public List<Vacancy> Vacancies { get; set; } = new();
+   //public List<Applicants> ApplicantsList { get; set; } = new();
 }

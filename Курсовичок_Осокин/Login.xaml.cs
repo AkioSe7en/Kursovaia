8bca981d599d0bdc0da20beedc164ddffcfb8432
Akio <myEmail@example.com>
2022-12-22 22:06:16 +0700

    Создание авторизации

---
+++ Курсовичок_Осокин/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace Куровичок_Осокин;
+
+public partial class Login : Window
+{
+    private ApplicationContext db = new ApplicationContext();
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void Reg_OnClick(object sender, RoutedEventArgs e)
+    {
+        // throw new NotImplementedException();
+    }
+
+    private void Auth_OnClick(object sender, RoutedEventArgs e)
+    {
+        var login = LoginBox.Text;
+        var password = PasswordBox.Text;
+        string errors = null;
+        var user = db.Users.Where(p => p.Login == LoginBox.Text).Where(p => p.Password == PasswordBox.Text)
+            .ToList();
+
+        if (user.Count==0)
+        {
+            MessageBox.Show("Неверный логин или пароль");
+        }
+        else
+        {
+            Login log;
+            switch (user[0].Level)
+            {
+                case "Консультант":
+                    MainWindow Kons = new MainWindow();
+                    Kons.Show();
+                    log = this;
+                    log.Close();
+                    break;
+                case "Рекрутер":
+                    Rekrut Rekr = new Rekrut();
+                    Rekr.Show();
+                    log = this;
+                    log.Close();
+                    break;
+                default:
+                    MessageBox.Show("Произошла непредвиденная ошибка. Обратитесь к системному администратору");
+                    break;
+            }
+        }
+    }
+}
---
+++ Курсовичок_Осокин/Tables/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Курсовичок_Осокин.Tables;
+
+public class Users
+{
+    public Guid UserID { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string Level { get; set; }
+}
